dd4c66b224ad577a2a19c63fef819e7d502be645
GermanBluefox <dogafox@gmail.com>
2026-09-01 10:29:57 +0300

    Ping says now when this host may not send ICMP at all instead of reporting every device as offline

--- admin/custom/customComponents.js
+++ admin/custom/customComponents.js
@@ -1,1 +1,12 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomPingSet__customComponents_js-DNdKKbIq.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomPingSet__customComponents_js-B-eejKrT.js";export{t as get,e as init};
+if (typeof document !== 'undefined' && document.head) {
+  try {
+    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__ConfigCustomPingSet__customComponents_js-B-eejKrT.js","assets/virtualExposes-CXP5qNLD.js","assets/virtual_mf-exposes___mfe_internal__ConfigCustomPingSet__customComponents_js-DbiI3mHD.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__ConfigCustomPingSet__mf_owner__1-CGgdSRdo.js","assets/vite-preload-helper-B7qeedMF.js"]) {
+      const __mfWarmupLink = document.createElement('link');
+      __mfWarmupLink.rel = 'modulepreload';
+      __mfWarmupLink.crossOrigin = '';
+      __mfWarmupLink.href = new URL(__mfWarmupPath, import.meta.url).href;
+      document.head.appendChild(__mfWarmupLink);
+    }
+  } catch (__mfWarmupError) {}
+}
